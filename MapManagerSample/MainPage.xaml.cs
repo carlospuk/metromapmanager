765ef93e28a447ba807b2015647798abcd9ab540
carlospuk <webstuff@fatattitude.com>
2012-07-09 21:56:34 +0100

    More sample code for callout buttons

--- MapManagerSample/MainPage.xaml.cs
+++ MapManagerSample/MainPage.xaml.cs
@@ -46,8 +46,10 @@ namespace MapUtilitiesSample
 
             // Initialise the MapManager, passing it a reference to our map
             mm = new MapManager(bingMap);
-            // We implement the feedback interface that says which map markers to use for each annotation
+            // We are the source that says which map markers to use for each annotation
             mm.markerSource = this;
+            // We're interested when a callout is tpped
+            mm.Callout_ButtonTapped += mm_Callout_ButtonTapped;
 
             // Add our sample data to the map - annotations and callouts will appear automatically
             mm.addAnnotations(restaurants);
@@ -56,6 +58,7 @@ namespace MapUtilitiesSample
             // You can also add polylines to the map from an object that supports the correct interface
             // mm.addPolyline(polylineSource, Windows.UI.Colors.Blue, 6);
         }
+
 
 
         // This method is where we tell the map what XAML UIElement to display for a given annotation
@@ -83,6 +86,22 @@ namespace MapUtilitiesSample
             }
             else
                 return null;
+        }
+
+
+        // Callout button tapped
+        void mm_Callout_ButtonTapped(object sender, CalloutButtonTappedEventArgs e)
+        {
+            // Show a message
+            headerLabel.Text =  "You tapped on " + e.Annotation.Title;
+
+            // You can Look at e.Annotation to figure out which object's callout was tapped
+            if (e.Annotation is Restaurant)
+            {
+                Restaurant tappedRestaurant = (Restaurant)e.Annotation;
+                // Do something with the object here
+            }
+
         }
 
 
